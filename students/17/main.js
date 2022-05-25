b01fec47bce7b3300cb3022240931b7f035f3dd7
Micheal Moore <mmoore@xpanxion.com>
2022-05-25 14:41:12 -0400

    Update

--- students/17/main.js
+++ students/17/main.js
@@ -3,31 +3,245 @@
 // Date: 5/24/2022
 // 
 
-const ex1 = () => {
+let ex1 = () => {
     const array = [1, '2', 3, 'test', 1.2];
     console.log(countNumbers(array));
 }
 
-const ex2 = () => {
-    // TODO...
+let ex2 = () => {
+    const array = [12, 55, 2, 22, 11];
+    console.log(minNumber(array));
 }
 
-const ex3 = () => {
-    // TODO...
+let ex3 = () => {
+    //usage 1
+    // const array1 = [1, 2, 3, 4, 5];
+    // const array2 = ['a', 'b', 'c', 'd', 'e'];
+    // console.log(interleave(array1, array2));
+    //usage 2
+    const array1 = [1, 2];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
 }
 
+let ex4 = () => {
+    console.log(palidrome('radar'));
+    console.log(palidrome('month'));
+}
+
+let ex5 = () => {
+    let str = "today this is a this is a this is a test.";
+    let newStr = str.split("this").length -1
+    console.log("'this' appears: " + newStr + " times")
+}
+
+let ex6 = () => {
+    let array = ['this', 'is', 'a', 'test', 'happy'];
+    console.log(longestString(array));
+}
+
+let ex7 = () => {
+    let n = [1, 3, 6, 3, 6, 10];
+    console.log(sort(n));
+}
+
+let ex8 = () => {
+    let words = "Count the words in this string";
+    console.log(countWords(words));
+}
+
+let ex9 = () => {
+    var a = "this counts the number of words that end in s";
+    console.log(countS(a));
+}
+
+let ex10 = () => {
+    var array = ["this", "is", "a", "test"];
+    console.log(countLetters(array));
+}
+
+let ex11 = () => {
+    let arr = ['dog', 3, 7, 'cat', 13, 'car'];
+    console.log(numbersOnly(arr));
+}
+
+let ex12 = () => {
+    const c = new Calculator();
+    c.add(1,2);
+    c.sub(4,1);
+    c.div(10,2);
+    c.mul(2,2)
+    console.log(c.getHistory())
+}
+
+let ex13 = () => {
+    let dict = [
+        { "firstName": "joe", "lastName": "smith", age: 10 },
+        { "firstName": "paul", "lastName": "simmon", age: 20 },
+        { "firstName": "fred", "lastName": "jones", age: 30 },
+    ]
+    let result = dict.map(a => a.age);
+    let sum = 0;
+    for (let i = 0; i < result.length; i++) {
+        sum += result[i];
+    }
+    console.log(sum)
+}
+
+let ex14 = () => {
+    let customers = [{
+        name: 'ABC Inc',
+        credit: 100
+    }, {
+        name: 'ACME Corp',
+        credit: 200
+    }, {
+        name: 'IoT AG',
+        credit: 300
+    }];
+    console.log(customers.find(c => c.credit === 200))
+}
+
+const main = async () =>{
+    // ex1();
+    // ex2();
+    // ex3();
+    // ex4();
+    // ex5();
+    // ex6();
+    // ex7();
+    // ex8();
+    // ex9();
+    // ex10();
+    // ex11();
+    // ex12();
+     ex13();
+    // ex14();
+}
 //
-// Your functions here...
+//Functions
 //
 
+//Ex. 1
 const countNumbers = (array) => {
-    let retval = 0;
-    // TODO...
-    return retval;
+   let arr = array.filter(el => typeof el === 'number');
+   return arr.length;
 }
 
-const main = async () => {
-    ex1();
+//Ex. 2
+const minNumber = (array) => {
+    // return Math.min.apply( Math, array)
+    for(let i = 0; i<1; i++) {
+        array.sort(function(a, b){return a -b});
+    }
+    return array[0]
 }
 
-main();
+//Ex. 3
+const interleave = (array1, array2) => {
+    let arr = [];
+    if(array1.length === array2.length){
+        for(let i = 0; i < array1.length; i++){
+            arr.push(array1[i], array2[i])
+        }
+        return arr.join();
+    }
+    else {
+        return "ERROR: Array length mismatch"
+    }
+}
+
+//Ex. 4
+const palidrome = (word) => {
+    let result = word.split("").reverse().join("");
+    if(word === result){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//
+//Ex. 5 is up in the let ex5.
+// 
+
+//Ex. 6
+const longestString = (array) => {
+    let long = "";
+    for(let i = 0; i < array.length; i++){
+        if(array[i].length > long.length){
+            long = array[i]
+        }
+    }
+    return long;
+}
+
+//Ex. 7
+const sort = (array) => {
+    return array.sort(function(a, b){return a -b})
+}
+
+//Ex. 8
+const countWords = (word) => {
+    return word.split(" ").length
+}
+
+//Ex. 9
+const countS = (word) => {
+    let newStr = word.split("s").length -1
+    return newStr
+}
+
+//Ex. 10
+const countLetters = (array) => {
+    let arr = array.join("")
+    return arr.length
+}
+
+//Ex. 11
+const numbersOnly = (array) => {
+    return array.filter(el => typeof el === 'number');
+}
+
+//Ex. 12
+class Calculator{
+    constructor(){
+       this.history = []
+    }
+    add(num1, num2){
+        const sum = num1 + num2;
+        this.history.push(`Add: ${num1} + ${num2} = ${sum}`);
+        return sum;
+    }
+    sub(num1, num2){
+        const sum = num1 - num2;
+        this.history.push(`Sub: ${num1} - ${num2} = ${sum}`);
+        return sum;
+    }
+    div(num1, num2){
+        const sum = num1 / num2;
+        this.history.push(`Div: ${num1} / ${num2} = ${sum}`);
+        return sum;
+    }
+    mul(num1, num2){
+        const sum = num1 * num2;
+        this.history.push(`Mul: ${num1} * ${num2} = ${sum}`);
+        return sum;
+    }
+    getHistory(){
+        return this.history.join('\r\n');
+    }
+}
+//
+//Ex. 13 is up in ex13().
+//
+
+//
+//Ex. 14 is up in ex14().
+//
+
+//
+//Main
+//
+main() 
