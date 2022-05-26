7b80e5aefbfae6fc1fb4daa011590e426b74bf59
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-05-26 16:21:32 -0400

    Ex 5 - 11

--- students/8/main.js
+++ students/8/main.js
@@ -24,6 +24,48 @@ const ex4 = () => {
     console.log(palindrome('month'));
 }
 
+const ex5 = () => {
+    let retval = 0;
+    let str = "today this is a this is a this is a test.";
+    const strArray = str.split(" ")
+    for (let i = 0; i < strArray.length; i++) {
+        if (strArray[i] == 'this') {
+            retval += 1
+        }
+    }
+    console.log("'This' appears " + retval + " times.")
+}
+
+const ex6 = () => {
+    const array = ['this', 'is', 'a', 'test', 'happy'];
+    console.log(longestString(array));
+}
+
+const ex7 = () => {
+    let n = [1, 3, 6, 3, 6, 10];
+    console.log(sort(n));
+}
+
+const ex8 = () => {
+    let words = "Count the words in this string";
+    console.log(countWords(words));
+}
+
+const ex9 = () => {
+    var a = "this counts the number of words that end in s";
+    console.log(countS(a));
+}
+
+const ex10 = () => {
+    var array = ["this", "is", "a", "test"];
+    console.log(countLetters(array));
+}
+
+const ex11 = () => {
+    let arr = ['dog', 3, 7, 'cat', 13, 'car'];
+    console.log(numbersOnly(arr));
+}
+
 //
 // Your functions here...
 //
@@ -68,8 +110,52 @@ const palindrome = (str) => {
     }
 }
 
+const longestString = (array) => {
+    const sorted = array.sort((a, b) => b.length - a.length)
+    return array[0].toString()
+}
+
+const sort = (n) => {
+    const arr = n.sort((function(a, b){return a - b}))
+    return arr
+}
+
+const countWords = (words) => {
+    const wordsArr = words.split(" ")
+    return wordsArr.length
+}
+
+const countS = (a) => {
+    let retval = 0;
+    const aArr = a.split(" ")
+    for (let i = 0; i < aArr.length; i++) {
+        if (aArr[i].includes("s")) {
+            retval += 1
+        }
+    }
+    return retval
+}
+
+const countLetters = (array) => {
+    let retval = 0;
+    for (let i = 0; i < array.length; i++) {
+        retval += array[i].length
+    }
+    return retval
+}
+
+const numbersOnly = (arr) => {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == 'number') [
+            newArr.push(arr[i])
+        ]
+    }
+    return newArr
+}
+
 const main = async () => {
-    ex4()
+    ex11()
 }
 
 main();
