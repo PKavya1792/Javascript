eb071f0d1ed7b1292438f558e689b12d1bd24c0a
Connor Shillady <c.shillady24@gmail.com>
2022-05-26 11:20:09 -0400

    Exercise 3

--- students/5/main.js
+++ students/5/main.js
@@ -3,24 +3,12 @@
 // Date: 5/24/2022
 // 
 
+//ex 1
 const ex1 = () => {
     const array = [1, '2', 3, 'test', 1.2];
     console.log(countNumbers(array));
 }
-
-const ex2 = () => {
-    const array = [12, 55, 2, 22, 11];
-    console.log(minNumber(array));
-}
-
-const ex3 = () => {
-    // TODO...
-}
-
-//
-// Your functions here...
-//
-
+//func 1
 const countNumbers = (array) => {
     let retval = 0;
     const COMPARISON_NUMBER = 1;
@@ -31,6 +19,12 @@ const countNumbers = (array) => {
     } return retval;
 }
 
+//ex 2
+const ex2 = () => {
+    const array = [12, 55, 2, 22, 11];
+    console.log(minNumber(array));
+}
+//func 2
 const minNumber = array => {
     let retval = array[0];
     for (let x of array) {
@@ -40,9 +34,30 @@ const minNumber = array => {
     } return retval;
 }
 
+//ex 3
+const ex3 = () => {
+    const array1 = [1, 2, 3, 4, 5];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    // const array1 = [1, 2];
+    // const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
+}
+//func 3
+const interleave = (array1, array2) => {
+    let result = "";
+    if (array1.length == array2.length) {
+        for (let i = 0; i < array2.length; i++) {
+            result += array1[i] + array2[i];
+        }
+    } else if (array1.length != array2.length) {
+        result = "ERROR: Array length mismatch";
+    } return result;
+}
+
 const main = async () => {
     //ex1();
-    ex2();
+    //ex2();
+    ex3();
 }
 
 main();
