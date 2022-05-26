542fe3f55d62a3d31bdea810af26e558f065f6d7
Joel Sipin <jsipin@xpanxion.com>
2022-05-26 15:26:49 -0400

    update

--- students/32/main.js
+++ students/32/main.js
@@ -3,40 +3,63 @@ const ex1 = () => {
     console.log(countNumbers(array));
   }
   
-  const ex2 = () => {
+    const ex2 = () => {
     const array = [12, 55, 2, 11];
     console.log(minNumber(array));
     
-    // TODO...
+   
   }
   
-  const ex3 = () => {
-    // TODO...
+    const ex3 = () => {
+    const array1 = [1, 2, 3, 4, 5, 1];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
+
   }
   
-  //
-  // Your functions here...
-  //
-  
-  const minNumber = (array) => {
-    let retval = array[0];
+  const countNumbers = (array) => {
+    let retval = 0;
     for(let i of array){
-        if(i<retval)
-        {retval=i;
-           
+        if(typeof(i)=='number'){
+            retval++
         }
-                        
-        
      
       
     }
+    
+    return retval;
+}
+  const minNumber = (array) => {
+    let retval = array[0];
+    for(let i of array){
+        if(i<retval){
+            retval=i;
+           
+        }
+                        
+   }
 
     
     return retval;
 } 
   
+
+
+
+
+  const interleave = (array1, array2) => {
+    
+    if(array1.length != array2.length){
+        return `ERROR: Array length mismatch`
+    }
+    else{
+        
+    } 
+   
+} 
+  
   const main = async () => {
-    ex2();
+    ex3();
   }
   
   main();
