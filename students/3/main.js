57183b149f85b3c1f4c0d3b3ed3118c7f9510c33
Brianprice098 <103675908+Brianprice098@users.noreply.github.com>
2022-05-26 16:44:22 -0400

    ex2 and ex3 done

--- students/3/main.js
+++ students/3/main.js
@@ -10,10 +10,19 @@ const ex1 = () => {
 }
 
 const ex2 = () => {
+    const array = [12, 55, 2, 22, 11];
+    console.log(minNumber(array));
     // TODO...
 }
 
 const ex3 = () => {
+    //const array1 = [1, 2, 3, 4, 5];
+    //const array2 = ['a', 'b', 'c', 'd', 'e'];
+    //console.log(interleave(array1, array2));
+
+    const array1 = [1, 2];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
     // TODO...
 }
 
@@ -33,9 +42,41 @@ const countNumbers = (array) => {
     // TODO...
     return retval;
 }
+const minNumber = (array) => {
+    let retval = 0;
+        for (const number of array){
+            if (number < retval){
+                retval = number
+            }
+            else if(retval == 0){
+                retval = number
+            }
+        }
+        return  retval
+    
+}
+const interleave = (array1, array2) =>{
+    let length = 0;
+    let retval = ""
+    if(array1.length != array2.length){
+        return "Error: Array length mismatch"
+    }
+    else{
+        length = array1.length;
+    }
+    for(let i = 0; i < length; i++){
+        retval += array1[i]
+        retval += array2[i]
+    }
+    return retval
+}
+
 
 const main = async () => {
     ex1();
+    ex2();
+    ex3();
 }
+
 
 main();
