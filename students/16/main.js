353701f723cf5742d0904635a298052ad6527583
Michael Leahey <michael.leahey1843@outlook.com>
2022-05-26 12:01:50 -0400

    ex2

--- students/16/main.js
+++ students/16/main.js
@@ -3,16 +3,18 @@
 // Date: 5/24/2022
 // 
 
-const ex1 = () => {
+//const ex1 = () => {
    
-    const array = [1, '2', 3, 'test', 1.2];
+  //  const array = [1, '2', 3, 'test', 1.2];
    
-    console.log(countNumbers(array));
-}
-
-//const ex2 = () => {
-    // TODO...
+    //console.log(countNumbers(array));
 //}
+
+const ex2 = () => {
+    const array = [12, 55, 2, 22, 11];
+
+console.log(minNumber(array));
+}
 
 //const ex3 = () => {
     // TODO...
@@ -33,10 +35,33 @@ const countNumbers = (array) => {
     }
      return retval;
 }
+//Ex 2 starts here
+//const array = [12, 55, 2, 22, 11];
+//console.log(minNumber(array));
+const minNumber = (array) => {
+    //use the first number then will loop
+    //all of them and pick the lowest
+    //arrays always needs to be checked against
+    //something that is why [0]
+    let retval = array [0]
+    for(const i of array){
+        if((i) < retval){
+            retval = i
+        }
+    }
+    return retval
+
+}
 
 const main = async () => {
 
-    ex1();
+    ex2();
 }
 
 main();
+
+
+
+//for(let i = 0; i < 3; i--){
+ //   console.log ('test')
+//}
