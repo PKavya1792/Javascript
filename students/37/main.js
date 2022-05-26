01ac874f724029eceee7471a174a7d891a73252b
Sherika Fayson <sfayson@xpanxion.com>
2022-05-26 16:25:21 -0400

    update

--- students/37/main.js
+++ students/37/main.js
@@ -9,25 +9,56 @@ const ex1 = () => {
 }
 
 const ex2 = () => {
-    // TODO...
+    const array =[12, 55,2,22,11];
+    console.log(minNumber(array));
 }
 
-const ex3 = () => {
-    // TODO...
+const ex3 = () =>{
+const array1 = [1, 2, 3, 4, 5];
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+console.log(interleave(array1, array2));
 }
 
 //
 // Your functions here...
 //
 
+const interleave = (array1, array2) => {
+    let retval = 'ERROR: Array length mismatch';
+    if (array1.length == array2.length) {
+        retval = '';
+        for (let i = 0; i < array1.length; i++) {
+            retval += array1[i] + array2[i];
+        }
+    }
+    return retval;
+}
+const minNumber = (array) => {
+
+    let retval = array[0];
+
+    for(let i of array){
+
+        if(i<retval){
+
+            retval=i;
+        }
+   }
+    return retval;
+}
 const countNumbers = (array) => {
     let retval = 0;
-    // TODO...
+    for (let i of array){
+        if(typeof(i)=='number'){
+            retval++
+        }
+        
+    }
     return retval;
 }
 
 const main = async () => {
-    ex1();
+    ex2();
 }
 
 main();
