95aca6faacaaf4eb6f659b0b476bb8b5d745a806
progammingwithalanfernandez301@gmail.ccom <afernandez@xpanxion.com>
2022-05-26 12:02:34 -0400

    update

--- students/28/main.js
+++ students/28/main.js
@@ -9,7 +9,8 @@ const ex1 = () => {
 }
 
 const ex2 = () => {
-    // TODO...
+    const array = [12, 55, 2, 22, 11];
+    console.log(minNumber(array));
 }
 
 const ex3 = () => {
@@ -19,6 +20,16 @@ const ex3 = () => {
 //
 // Your functions here...
 //
+const minNumber = (array) => {
+    let retval = array[0];
+    for (let i of array){
+        if(i < retval){
+            retval = i
+        }
+    }
+    return retval 
+} 
+
 
 const countNumbers = (array) => {
     let retval = 0;
@@ -31,7 +42,7 @@ const countNumbers = (array) => {
 }
 
 const main = async () => {
-    ex1();
+    ex2();
 }
 
 main();
