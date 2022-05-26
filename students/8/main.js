8b9336561c40914bc6f03b308079873d160cf9f4
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-05-26 12:17:26 -0400

    Ex 2, 3, & 4

--- students/8/main.js
+++ students/8/main.js
@@ -4,34 +4,72 @@
 // 
 
 const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
+    const array1 = [1, '2', 3, 'test', 1.2];
+    console.log(countNumbers(array1));
 }
 
 const ex2 = () => {
-    // TODO...
+    const array2 = [12, 55, 2, 22, 11];
+    console.log(minNumber(array2));
 }
 
 const ex3 = () => {
-    // TODO...
+    const array3 = [1, 2, 3, 4, 5];
+    const array4 = ['a','b','c','d','e'];
+    console.log(interleave(array3, array4));   
+}
+
+const ex4 = () => {
+    console.log(palindrome('radar'));
+    console.log(palindrome('month'));
 }
 
 //
 // Your functions here...
 //
 
-const countNumbers = (array) => {
+const countNumbers = (array1) => {
     let retval = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (typeof(array[i]) === 'number') {
+    for (let i = 0; i < array1.length; i++) {
+        if (typeof(array1[i]) === 'number') {
             retval += 1
         }
     }
-    console.log(retval)
+    return retval
+}
+
+const minNumber = (array2) => {
+    array2.sort(function(a, b){return a - b});
+    return array2[0]
+}
+
+const interleave = (array3, array4) => {
+    let newArray = [];
+    for (let i = 0; i < array4.length; i++) {
+        newArray.push(array3[i], array4[i])
+    }
+
+    if (newArray.includes(undefined)) {
+        return "ERROR: Array length mismatch"
+    } else {
+        return newArray.toString()
+    }
+}
+
+const palindrome = (str) => {
+    let normalArray = str.split("")
+    let reversedArray = str.split("").reverse()
+    let normal = normalArray.join("")
+    let reversed = reversedArray.join("")
+    if (normal == reversed) {
+        return true
+    } else {
+        return false
+    }
 }
 
 const main = async () => {
-    ex1();
+    ex4()
 }
 
 main();
