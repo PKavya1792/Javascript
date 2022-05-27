f6eb705fdd063dcbf6a5fca14ed893912a8f0e43
progammingwithalanfernandez301@gmail.ccom <afernandez@xpanxion.com>
2022-05-27 13:58:17 -0400

    update

--- students/28/main.js
+++ students/28/main.js
@@ -7,12 +7,10 @@ const ex1 = () => {
     const array = [1, '2', 3, 'test', 1.2];
     console.log(countNumbers(array));
 }
-
 const ex2 = () => {
     const array = [12, 55, 2, 22, 11];
     console.log(minNumber(array));
 }
-
 const ex3 = () => {
     const array1 = [1, 2, 3, 4, 5, 6];
     const array2 = ['a', 'b', 'c', 'd', 'e'];
@@ -22,9 +20,26 @@ const ex4 = () => {
     console.log(palidrome('radar'));
     console.log(palidrome('month'));
 }
+const ex5 = () => {
+    const text = "today this is a this is a this is a test.";
+    console.log(countOccurences(text, 'this'))
+}
+
 //
 // Your functions here...
 //
+const countOccurences = (text, searchWord) => {
+    let retval = ''
+    let wordCounter = 0
+    const wordArray = text.split(" ")
+        for (let word of wordArray) {
+            if (searchWord == word) {
+            wordCounter ++    
+        }        
+    }
+    retval = "this appears: " + wordCounter + " times"
+    return retval
+}
 
 const palidrome = (word) => {
     let retval = true
@@ -33,13 +48,12 @@ const palidrome = (word) => {
     const reverseArray = input.reverse("")
     const reversedWord = reverseArray.join("")
 
-if(reversedWord !== word)  {
+    if(reversedWord !== word)  {
     retval = false
 }
 
     return retval
 }
-
 
 
 const interleave = (array1, array2) => {
@@ -72,7 +86,7 @@ const countNumbers = (array) => {
 }
 
 const main = async () => {
-    ex4();
+    ex5();
 }
 
 main();
