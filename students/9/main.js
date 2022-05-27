5c59fdfab02b4dd5a8727eaf29ba14787af2c925
Donna Lemon <donnalemon1@gmail.com>
2022-05-26 23:22:07 -0400

    Ex. 3

--- students/9/main.js
+++ students/9/main.js
@@ -2,66 +2,47 @@
 // File: main.js
 // Date: 5/24/2022
 //
+/*Usage #1:
+const array1 = [1, 2, 3, 4, 5];
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+console.log(interleave(array1, array2));
+Output: 1a2b3c4d5e
 
-//Output: 2
+Usage #2:
+const array1 = [1, 2];
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+console.log(interleave(array1, array2));
+Output: ERROR: Array length mismatch*/
 
-const ex2 = () => {
-  const array = [12, 55, 2, 22, 11];
-  console.log(minNumber(array));
+const ex3 = () => {
+  const array1 = [1, 2, 3, 4, 5];
+  const array2 = ["a", "b", "c", "d", "e"];
+  console.log(interleave(array1, array2));
 };
-
-const minNumber = (array) => {
-  let retval = array(0);
-  for (let i of array) {
-    if (i < retval) {
-      retval = i;
-    }
-  }
-  return retval;
-};
-
-const countNumbers = (array) => {
-  let retval = 0;
-  for (let i of array) {
-    if (typeof i == "number") {
-      retval++;
-    }
-  }
-};
-const main = async () => {
-  ex2();
-};
-
-main();
-
-/*const ex3 = () => {
-    const array1 = [1, 2, 3, 4, 5];
-    const array2 = ['a', 'b', 'c', 'd', 'e'];
-    console.log(interleave(array1, array2));
-}
 
 //
 // Your functions here...
-//*/
+//
 
 /*const countNumbers = (array) => {
     let retval = 0;
     You are Here ...
     return retval;
-}
+}*/
 
 const interleave = (array1, array2) => {
-    let retval = 'ERROR: Array length mismatch';
-    if (array1.length == array2.length) {
-        retval = '';
-        for (let i = 0; i < array1.length; i++) {
-            retval += array1[i] + array2[i];
-        }
+  let retval = "";
+  if (array1.length != array2.length) {
+    retval = "ERROR: Array length mismatch";
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      retval += array1[i] + array2[i];
     }
     return retval;
-}
+  }
+};
 const main = async () => {
-    ex3();
-}
+  ex3();
+};
 
-main();*/
+main();
