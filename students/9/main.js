6c1db029c58c3d1ce514444e02378df18af3ffc7
Donna Lemon <donnalemon1@gmail.com>
2022-05-26 22:50:48 -0400

    Ex. 1

--- students/9/main.js
+++ students/9/main.js
@@ -1,33 +1,56 @@
 //
 // File: main.js
 // Date: 5/24/2022
-// 
+//
 
-const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
-}
+const countNumbers = (array) => {
+  let retval = 0;
+  for (let i of array) {
+    if (typeof i == "number") {
+      retval++;
+    }
+  }
+  return retval;
+};
 
-const ex2 = () => {
+const main = async () => {
+  ex1();
+};
+
+main();
+
+/*const ex2 = () => {
     // TODO...
 }
 
 const ex3 = () => {
-    // TODO...
+    const array1 = [1, 2, 3, 4, 5];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
 }
 
 //
 // Your functions here...
-//
+//*/
 
-const countNumbers = (array) => {
+/*const countNumbers = (array) => {
     let retval = 0;
-    // TODO...
+    You are Here ...
     return retval;
 }
 
+const interleave = (array1, array2) => {
+    let retval = 'ERROR: Array length mismatch';
+    if (array1.length == array2.length) {
+        retval = '';
+        for (let i = 0; i < array1.length; i++) {
+            retval += array1[i] + array2[i];
+        }
+    }
+    return retval;
+}
 const main = async () => {
-    ex1();
+    ex3();
 }
 
-main();
+main();*/
