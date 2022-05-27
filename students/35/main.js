c1c4da6b21ebd94271ec1afecdc43b12c1416107
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-05-27 15:59:24 -0400

    update

--- students/35/main.js
+++ students/35/main.js
@@ -39,17 +39,45 @@ const ex7 = () => {
     console.log(sort(arrayOfNumbers));
 }
 
+const ex8 = () => {
+    const words = "Count the words in this string";
+    console.log(countWords(words));
+}
+
+const ex9 = () => {
+    const a = "this counts the number of words that end in s";
+    console.log(countS(a));
+}
 //
 // Your functions here...
 //
 
+//Ex 9
+
+const countS = (a) => {
+
+}
+//Ex 8
+const countWords = (words) => {
+    let retval = 0;
+    const array = words.split(" ");
+    for (let i of array) {
+      if (typeof(i) =='string') {
+        retval++;
+      }
+    } 
+    return retval;
+}
+
+
+//Ex 7
 const sort = (arrayOfNumbers) => {
     const sortedArrayOfNumbers = arrayOfNumbers.sort((a,b) => (a-b))
     return sortedArrayOfNumbers
 }
 
 
-
+//Ex 6
 const longestString = (array) => {
     let lgth = 0;
     let longest;    
@@ -63,46 +91,47 @@ const longestString = (array) => {
 }
 
 
-
+//Ex 5
 const countOccurences = (text, searchWord) => {
     let retval = ""
     let wordCounter = 0;
     const wordArray = text.split(" ");
     for (let word of wordArray) {
         if (searchWord == word) {
-        wordCounter++
+           wordCounter++
         }
     }
     retval = "This appears: " + wordCounter + " times"
     return retval;
 }
 
+//Ex 4
 const palindrome = (word) => {
     let retval=true;
     const input = word.split("");
     const reverseArray = input.reverse("");
     const reversedWord= reverseArray.join("");
         if (reversedWord !== word) {
-        retval = false;
+           retval = false;
         }
     return retval;
 }
 
+//Ex 3
 const interleave = (array1, array2) => {
     if (array1.length != array2.length){
         return 'ERROR: Array length mismatch'
     }
     else {
         let text="";
-        for(let i = 0; i < array1.length; i++)
-            {
-                text += array1[i] + array2[i]
-            }
-               return text
+        for(let i = 0; i < array1.length; i++) {
+            text += array1[i] + array2[i]
+        }
+        return text
     }
 }
  
-
+//Ex 2
 const minNumber = (array) => {
     let retval = array[0]
     for (let i of array) {
@@ -113,18 +142,19 @@ const minNumber = (array) => {
       return retval;
 }
 
+//Ex 1
 const countNumbers = (array) => {
     let retval = 0;
     for (let i of array) {
-      if (typeof(i) =='number')
-      retval++;
+      if (typeof(i) =='number') {
+        retval++;
+      }
     } 
-
     return retval;
 }
 
 const main = async () => {
-    ex7();
+    ex8();
 }
 
 main();
