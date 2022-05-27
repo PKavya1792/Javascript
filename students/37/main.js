deeb9b5ac254b22721d648ad8694166f4b157195
Sherika Fayson <sfayson@xpanxion.com>
2022-05-27 11:31:38 -0400

    update

--- students/37/main.js
+++ students/37/main.js
@@ -1,64 +1,77 @@
 //
 // File: main.js
 // Date: 5/24/2022
-// 
+//
 
 const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
-}
+  const array = [1, "2", 3, "test", 1.2];
+  console.log(countNumbers(array));
+};
 
 const ex2 = () => {
-    const array =[12, 55,2,22,11];
-    console.log(minNumber(array));
-}
+  const array = [12, 55, 2, 22, 11];
+  console.log(minNumber(array));
+};
 
-const ex3 = () =>{
-const array1 = [1, 2, 3, 4, 5];
-const array2 = ['a', 'b', 'c', 'd', 'e'];
-console.log(interleave(array1, array2));
-}
+const ex3 = () => {
+  const array1 = [1, 2, 3, 4, 5];
+  const array2 = ["a", "b", "c", "d", "e"];
+  console.log(interleave(array1, array2));
+};
+
+const ex4 = () => {
+  console.log(palindrome("radar"));
+  console.log(palindrome("month"));
+};
 
 //
 // Your functions here...
 //
 
+const palindrome = (word) => {
+  let retval = true;
+  const input = word.split("");
+  const reverseArray = input.reverse("");
+  const reverseWord = reverseArray.join("");
+  if (reverseWord !== word) {
+    retval = false;
+  }
+  return retval;
+};
+
 const interleave = (array1, array2) => {
-    let retval = 'ERROR: Array length mismatch';
-    if (array1.length == array2.length) {
-        retval = '';
-        for (let i = 0; i < array1.length; i++) {
-            retval += array1[i] + array2[i];
-        }
+  let retval = "ERROR: Array length mismatch";
+  if (array1.length == array2.length) {
+    retval = "";
+    for (let i = 0; i < array1.length; i++) {
+      retval += array1[i] + array2[i];
     }
-    return retval;
-}
+  }
+  return retval;
+};
+
 const minNumber = (array) => {
-
-    let retval = array[0];
-
-    for(let i of array){
-
-        if(i<retval){
-
-            retval=i;
-        }
-   }
-    return retval;
-}
-const countNumbers = (array) => {
-    let retval = 0;
-    for (let i of array){
-        if(typeof(i)=='number'){
-            retval++
-        }
-        
+  let retval = array[0];
+  for (let i of array) {
+    if (i < retval) {
+      retval = i;
     }
-    return retval;
-}
+  }
+  return retval;
+};
+
+const countNumbers = (array) => {
+  let retval = 0;
+  for (let i of array) {
+    if (typeof i == "number") {
+      retval++;
+    }
+  }
+  return retval;
+};
 
 const main = async () => {
-    ex2();
-}
+  ex4();
+};
 
 main();
