c7db06adbc8a9c8e85b4df24aefaaf39652318e3
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-05-27 15:53:01 -0400

    Ex1

--- students/11/main.js
+++ students/11/main.js
@@ -1,19 +1,19 @@
-//..
+//
 // File: main.js
 // Date: 5/24/2022
-// 
+//
 
 const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array));
+  const array = [1, "2", 3, "test", 1.2];
+  console.log(countNumbers(array));
 }
 
 const ex2 = () => {
-    // TODO...
+  // TODO...
 }
 
 const ex3 = () => {
-    // TODO...
+  // TODO...
 }
 
 //
@@ -21,13 +21,17 @@ const ex3 = () => {
 //
 
 const countNumbers = (array) => {
-    let retval = 0;
-    // TODO...
-    return retval;
+  let retval = 0;
+  for (let i of array) {
+    if (typeof i == "number") {
+      retval++;
+    }
+  }
+  return retval;
 }
 
 const main = async () => {
-    ex1();
+  ex1();
 }
 
 main();
