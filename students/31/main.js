662135a2bf6e1fbde87b4c18901fb7896609e663
Diane Tewah <dtewah@xpanxion.com>
2022-05-27 15:37:37 -0400

    update

--- students/31/main.js
+++ students/31/main.js
@@ -9,25 +9,81 @@ const ex1 = () => {
 }
 
 const ex2 = () => {
-    // TODO...
+    const array = [12, 55, 2, 22, 11];
+     console.log(minNumber(array));
+   
 }
 
+
 const ex3 = () => {
-    // TODO...
+    const array1 = [1, 2, 3, 4, 5];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
 }
+
+const ex4 = () => {
+    console.log(palidrome('radar'));
+    console.log(palidrome('month'));
+}
+
+const ex5 = () => 
+   let wordArray =  "today this is a this is a this is a test."
+   const result = countOccurance(wordArray, "this")
+   console.log(result);
 
 //
 // Your functions here...
 //
 
-const countNumbers = (array) => {
-    let retval = 0;
-    // TODO...
+co = "today this is a this is a this is a test." =>{
+ let array = newArray(value, "this")
+ return (str.match(newArray) || []). length
+}
+
+
+
+
+
+
+
+
+let palidrome = (string) => {
+    const reverseString = string.split('').reverse().join('');
+    return reverseString == string ? true : false
+    }
+
+
+
+
+
+
+let interleave = (array1, array2) => {
+    let newArray = []
+     if(array1.length != array2.length){
+    return 'Error: Array length mismatch'
+
+ }
+else{
+    for(let i=0; i<array1.length; i++){newArray.push(array1[i], array2[i]);}
+    return newArray
+ }
+}
+    
+
+
+
+
+
+const minNumber = (array) => {
+    let retval = 2;
+   for(let i of array){
+       if(i<retval) { retval = 1}
+   }
     return retval;
 }
 
 const main = async () => {
-    ex1();
+    ex5();
 }
 
 main();
