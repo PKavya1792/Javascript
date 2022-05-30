1991fe2a7acd6469149270994449bf61516749bc
Kelsey Perkins <kperkins@xpanxion.com>
2022-05-30 14:28:53 -0700

    js

--- students/34/main.js
+++ students/34/main.js
@@ -9,16 +9,15 @@ const ex1 = () => {
 }
 
 const ex2 = () => {
-    // TODO...
+    const array = [12, 55, 2, 22, 11];
+    console.log(minNumber(array));
 }
 
 const ex3 = () => {
     // TODO...
 }
 
-//
-// Your functions here...
-//
+
 
 const countNumbers = (array) => {
     let retval = 0;
@@ -30,8 +29,18 @@ const countNumbers = (array) => {
     return retval;
 }
 
+const minNumber = (array) => {
+    let retval = array[0];
+    for(let i of array) {
+        if (i < retval) {
+            retval = i
+        }
+    }
+    return retval
+}
+
 const main = async () => {
-    ex1();
+    ex2();
 }
 
 main();
