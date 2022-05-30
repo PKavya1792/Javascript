7511a99dec4aa4598ad5d6e7dfa93d86d656c295
Kelsey Perkins <kperkins@xpanxion.com>
2022-05-30 15:46:45 -0700

    js

--- students/34/main.js
+++ students/34/main.js
@@ -8,27 +8,13 @@ const ex1 = () => {
     console.log(countNumbers(array));
 }
 
+const countNumbers = (array) => {
+}
+
 const ex2 = () => {
     const array = [12, 55, 2, 22, 11];
     console.log(minNumber(array));
 }
-
-const ex3 = () => {
-    // TODO...
-}
-
-
-
-const countNumbers = (array) => {
-    let retval = 0;
-    for (let i of array){
-            if (typeof(i) =='number') {
-                retval++;
-            }
-       }
-    return retval;
-}
-
 const minNumber = (array) => {
     let retval = array[0];
     for(let i of array) {
@@ -39,8 +25,45 @@ const minNumber = (array) => {
     return retval
 }
 
+const ex3 = () => {
+    const array1 = [1, 2, 3, 4, 5];
+    const array2 = ['a', 'b', 'c', 'd', 'e'];
+    console.log(interleave(array1, array2));
+}
+
+const interleave = (array1, array2) => {
+    var word = ' ';
+    for(let i = 0; i < array1.length; i++) {
+        word += array1[i] + array2[i]
+    }
+    return word;
+}
+
+const ex4 = () => {
+    console.log(palidrome('radar'));
+    console.log(palidrome('month')); 
+}
+
+const ex5 = () => {
+    let str = "today this is a this is a this is a test.";
+    let thisWord = 'this';
+    console.log(countOccurnece(str, thisWord));
+}
+
+const countOccurnece = (str, thisWord) => {
+    let count = 0;
+    let answer = '';
+    const wordArray = str.split(' ');
+    for(let word of wordArray ) {
+        if (thisWord == word ){
+            count++
+        }
+    }
+    answer = 'this appears: ' + count + ' times'
+    return answer;
+}
 const main = async () => {
-    ex2();
+    ex5();
 }
 
 main();
